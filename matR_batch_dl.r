5f20e50f4d0dd8cd637a519e2681acdd26988499
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-02-04 10:50:15 -0600

    working version of matR_batch_dl.r uploaded

--- matR_batch_dl.r
+++ matR_batch_dl.r
@@ -1,4 +1,4 @@
-matR_batch_dl <<- function(mgid_list, batch_size = 50, my_entry="count", my_annot="func", my_source="Subsystem", my_level="level3"){
+matR_batch_dl <<- function(mgid_list, batch_size = 50, my_entry="count", my_annot="func", my_source="Subsystem", my_level="level3", debug=TRUE){
 
   require(matR)
   source_https("https://raw.github.com/braithwaite/matR-apps/master/collection-merge.R")
@@ -6,29 +6,33 @@ matR_batch_dl <<- function(mgid_list, batch_size = 50, my_entry="count", my_anno
   if ( exists("my_data")==TRUE ){rm(my_data)}
   
   num_batch <- as.integer( length(mgid_list)%/%batch_size )
+  if (debug==TRUE) {print(paste("num_batch:", num_batch))}
   batch_remainder <- length(mgid_list)%%batch_size
-  
+  if (debug==TRUE) {print(paste("batch_remainder:", batch_remainder))}
+        
   for (batch_count in 1:(num_batch)){
     
     if (batch_count == 1){
       print.tic()
       batch_start <- 1
       batch_end <- batch_size
+      if (debug==TRUE) {print(paste("first batch      -- batch_start:", batch_start, ":: batch_end:", batch_end))}
       batch_list = mgid_list[batch_start:batch_end]
       first_batch <- collection(batch_list, count = c(entry=my_entry, annot=my_annot, source=my_source, level=my_level))
       #first_batch.counts <- first_batch$count
       #my_data <<- first_batch.counts
       my_data <<- first_batch
-      print(paste("finished with batch", batch_count))
+      print(paste("finished with batch", batch_count, ":: with", (batch_end - batch_start + 1), "metagenomes" ))
       print.toc()
     }else{
       print.tic()
-      batch_start <- (batch_count*batch_size)+1
-      batch_end <- (batch_count*(batch_size+1))-1
+      batch_start <- ((batch_count-1)*batch_size)+1
+      batch_end <- (batch_count*batch_size)
+      if (debug==TRUE) {print(paste("continuing batch -- batch_start:", batch_start, ":: batch_end:", batch_end))}
       batch_list = mgid_list[batch_start:batch_end]
-      next_batch <- collection(batch_list, my_count = c(entry=my_entry, annot=my_annot, source=my_source, level=my_level))
+      next_batch <- collection(batch_list, count = c(entry=my_entry, annot=my_annot, source=my_source, level=my_level))
       my_data <<- my_data + next_batch
-      print(paste("finished with batch", batch_count))
+      print(paste("finished batch", batch_count, ":: with", (batch_end - batch_start + 1), "metagenomes"  ))
       print.toc()
     }
   }
@@ -37,10 +41,11 @@ matR_batch_dl <<- function(mgid_list, batch_size = 50, my_entry="count", my_anno
     print.tic()
     batch_start <- (num_batch*batch_size)+1
     batch_end <- length(mgid_list)
+    if (debug==TRUE) {print(paste("last batch         -- batch_start:", batch_start, ":: batch_end:", batch_end))}
     batch_list = mgid_list[batch_start:batch_end]
-    last_batch <- collection(batch_list, my_count = c(entry=my_entry, annot=my_annot, source=my_source, level=my_level))
+    last_batch <- collection(batch_list, count = c(entry=my_entry, annot=my_annot, source=my_source, level=my_level))
     my_data <<- my_data + last_batch
-    print(paste("finished with batch", batch_count))
+    print(paste("finished batch", batch_count, ":: with", (batch_end - batch_start + 1), "metagenomes"  ))
     print.toc()
   }
   
