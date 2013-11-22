108030e7fd0453b62c0fc3c09c4bd2d8bfcaac9a
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-11-22 15:46:09 -0600

    edit process_DRISEE2.pl

--- process_DRISEE2.11-18-13/process_DRISEE2.pl
+++ process_DRISEE2.11-18-13/process_DRISEE2.pl
@@ -115,7 +115,7 @@ my @summary_values = ($file_in); # array
 open(DRISEE_STDOUT, "<", $drisee_stdout) or die "\n\n"."can't open DRISEE_STDOUT $drisee_stdout"."\n\n";
 while (my $line = <DRISEE_STDOUT>){
     chomp $line;
-    if ($line =~ /^V|^bp|^se|^av|^st|^le|^In|^Pr|^Dr|^Con|^Dr/){ # skip comment lines
+    if ($line =~ /^V|^bp|^se|^av|^st|^le|^In|^Processed|^Dr|^Con|^Dr/){ # skip comment lines
       if($debug){ print STDOUT "\n"."line: ".$line; }
       #if ($line =~ /^Dr/){ # skip comment lines
       chomp $line;
