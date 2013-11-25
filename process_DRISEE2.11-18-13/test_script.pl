6131f2fcd50179b106c30b95beef25b2349d2688
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-11-25 12:11:43 -0600

    edits to the perl script

--- process_DRISEE2.11-18-13/test_script.pl
+++ process_DRISEE2.11-18-13/test_script.pl
@@ -3,6 +3,13 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use File::ReadBackwards;
+
+
+# use File::ReadBackwards
+# my $bw = File::ReadBackwards->new("some_file");
+# print reverse map { $bw->readline() } (1 .. 3);
+
 
 my $debug=1;
 
@@ -13,7 +20,7 @@ my $file_done = 0;
 while ($file_done == 0){
  
   sleep 10;
-  my $file_tail = system("tail -n 3 $file");
+  my $file_tail = system(`tail -n 3 $file`);
   if($debug){ print "\n\n"."last three lines out stdout:"."\n".$file_tail."\n"; }
   #my @file_tail_array = split("\n", $file_tail);
   #if ( @file_tail_array[1] =~ m/^Con/ ){ # consider the file to be done of the second to last line starts with "Con..taminated"
