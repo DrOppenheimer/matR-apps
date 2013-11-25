8935da8db73c51a5320071c2a3a4487f934b8a1c
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-11-25 11:25:19 -0600

    edits to the perl script

--- process_DRISEE2.11-18-13/process_DRISEE2.pl
+++ process_DRISEE2.11-18-13/process_DRISEE2.pl
@@ -160,8 +160,8 @@ while ($file_done == 0){
   sleep 10;
   my $file_tail = system("tail -n 3 $drisee_stdout");
   if($debug){ print "\n\n"."last three lines out stdout:"."\n"; }
-  my @file_tail = split("\n", $file_tail);
-  if ( $file_tail[1] =~ m/^Con/ ){ # consider the file to be done of the second to last line starts with "Con..taminated"
+  my @file_tail_array = split("\n", $file_tail);
+  if ( $file_tail_array[1] =~ m/^Con/ ){ # consider the file to be done of the second to last line starts with "Con..taminated"
     $file_done++;
   }
 }
