f6ed0931f5865a18ab3fede78abcb02aee9f4f50
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-11-25 11:59:50 -0600

    edits to the perl script

--- process_DRISEE2.11-18-13/test_script.pl
+++ process_DRISEE2.11-18-13/test_script.pl
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Getopt::Long;
 
+my $debug=1;
+
 my $file = "mgm4473069.3.fastq.drisee_stdout.txt"
 
 my $file_done = 0;
@@ -15,6 +17,7 @@ while ($file_done == 0){
   if($debug){ print "\n\n"."last three lines out stdout:"."\n"; }
   my @file_tail_array = split("\n", $file_tail);
   if ( $file_tail_array[1] =~ m/^Con/ ){ # consider the file to be done of the second to last line starts with "Con..taminated"
+    if($debug){print STDOUT "Second to last line: "."\n".$file_tail_array[1]."\n\n";}
     $file_done++;
   }
   
