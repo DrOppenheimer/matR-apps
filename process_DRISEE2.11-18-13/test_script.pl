70d24e3159be8a59303c3c2a28b28e52fe47e5ff
Kevin Keegan <kevin.p.keegan@gmail.com>
2013-11-25 12:20:56 -0600

    edits to the perl script

--- process_DRISEE2.11-18-13/test_script.pl
+++ process_DRISEE2.11-18-13/test_script.pl
@@ -17,19 +17,18 @@ my $file = "mgm4473069.3.fastq.drisee_stdout.txt";
 
 my $file_done = 0;
 
-#while ($file_done == 0){
+while ($file_done == 0){
  
   #sleep 10;
   #my $file_tail = system("tail -n 3 $file");
   
-  my $file_tail = `tail -n 10 $file`;  
+  my $file_tail = `tail -n 4 $file`;  
 
-  if($debug){ print "\n\n"."last three lines out stdout:"."\n".$file_tail."\n"; }
-
-#my @file_tail_array = split("\n", $file_tail);
-  #if ( @file_tail_array[1] =~ m/^Con/ ){ # consider the file to be done of the second to last line starts with "Con..taminated"
-  #  if($debug){print STDOUT "Second to last line: "."\n".$file_tail_array[1]."\n\n";}
-  #  $file_done++;
-  #}
+  #if($debug){ print "\n\n"."last three lines out stdout:"."\n".$file_tail."\n"; }
+  my @file_tail_array = split("\n", $file_tail);
+  if ( @file_tail_array[0] =~ m/^Con/ ){ # consider the file to be done of the second to last line starts with "Con..taminated"
+    if($debug){print STDOUT "Fourth to last line: "."\n".$file_tail_array[1]."\n\n";}
+    $file_done++;
+  }
   
-#}
+}
