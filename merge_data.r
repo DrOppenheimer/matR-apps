8111fe01bf3c4fea22aa8753c2f37ee6a4f1da24
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-02-13 16:28:50 -0600

    added data_merge function - simple tool to merge falt file or R object tables into one

--- merge_data.r
+++ merge_data.r
@@ -1,23 +1,28 @@
-merge_data <- functon(file1="", file2="", output=""){
+merge_data <- functon(mode="file", file1="", file2="", output="default"){
 
-  data1 = import_data(file1)
-
-  data2 = import_data(file2)
+  if( identical( output, "default" )==TRUE ){
+    output <- paste( file1, ".AND.", file2 ,sep="", collapse="" )
+  }
+  
+  # can merge from flat file or data matrices
+  if ( identical(mode, "file")==TRUE ){
+    data1 = import_data(file1)
+    data2 = import_data(file2)
+  }else{
+    data1 <- file1
+    data2 <- file2
+  }
 
   merged_data <- merge(data1, data2, by="row.names", all=TRUE)
 
-  merged_data
+  write.table(merged_data, file = output, col.names=NA, row.names = TRUE, sep="\t", quote=FALSE)
 
 }
+
+
 
 #### SUBS
-    import_data <- function(file_name)
+import_data <- function(file_name)
 {
- data.matrix(read.table(file_name, row.names=1, header=TRUE, sep="\t", comment.char="", quote="", check.names=FALSE))
-}
-
-
-
-
-    
+  data.matrix(read.table(file_name, row.names=1, header=TRUE, sep="\t", comment.char="", quote="", check.names=FALSE))
 }
