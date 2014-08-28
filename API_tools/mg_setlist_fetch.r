b334a5e142d388c0626d9090f6c6f338a3385d63
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-08-28 13:35:45 -0500

    added mg_setlist_fetch.r

--- API_tools/mg_setlist_fetch.r
+++ API_tools/mg_setlist_fetch.r
@@ -1,12 +1,21 @@
-mg_setlist_fetch <- function(mgid, print_setlist){
-    my_url <- paste("http://api.metagenomics.anl.gov//download/", mgid, "?name=setlist", sep="")
-    my_json <- fromJSON(getURL(my_url))  
-    if ( print_setlist==TRUE ){
-      for (i in 1:length(my_json)){ 
-        my_entry <- my_json[[i]][ c("id", "stage_id", "stage_name", "file_type", "file_name", "file_id", "url" ) ]
-        for (j in 1:length(my_entry)){ print(paste( names(my_entry)[j], "::", (my_entry)[j]))  }
-        print("##############################################")
-      }
-    }
-    return(my_json)
+mg_setlist_fetch <- function(mgid, print_setlist, auth="default"){
+  
+  require(matR)
+  require(RCurl)
+  require(RJSONIO)
+
+  if( identical(auth,"default") ){
+    auth <- msession$getAuth()
   }
+  
+  my_url <- paste("http://api.metagenomics.anl.gov//download/", mgid, "?name=setlist", "$auth=", auth, sep="")
+  my_json <- fromJSON(getURL(my_url))  
+  if ( print_setlist==TRUE ){
+    for (i in 1:length(my_json)){ 
+      my_entry <- my_json[[i]][ c("id", "stage_id", "stage_name", "file_type", "file_name", "file_id", "url" ) ]
+      for (j in 1:length(my_entry)){ print(paste( names(my_entry)[j], "::", (my_entry)[j]))  }
+      print("##############################################")
+    }
+  }
+  return(my_json)
+}
