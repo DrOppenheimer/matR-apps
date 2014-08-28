05d30cd1c5a73709bbb5580e46603c08604969bf
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-08-28 15:30:04 -0500

    added mg_setlist_fetch.r

--- API_tools/download_for_qiime.r
+++ API_tools/download_for_qiime.r
@@ -48,7 +48,7 @@ download_for_qiime <- function(mgid_list="./test_id_list.txt", my_stage_name="de
   }
 
   if( cleanup==TRUE ){
-    for (i in nrow(mapping_matrix)){
+    for (i in 1:nrow(mapping_matrix)){
       file_to_delete <- paste(my_destination_dir, "/", mapping_matrix[i,4] , sep="")
       if(debug==TRUE){ print(paste("file_to_delete:",file_to_delete)) }
       if( file.exists(file_to_delete) ){ unlink(file_to_delete) }
