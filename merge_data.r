4e7c27c7929dc47731d0a646f23c80df5e47630f
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-09-02 14:32:08 -0500

    fixed merge.r to handle data or metadata

--- merge_data.r
+++ merge_data.r
@@ -27,17 +27,17 @@ merge_data <- function(mode="file", data_type = "data", file1="", file2="", outp
     data2 <- file2
   }
 
-  #if  (identical(data_type, "data")){
+  if  (identical(data_type, "data")){
     merged_data <- merge(data1, data2, by="row.names", all=TRUE, suffixes=(c("rep1","rep2")))
     rownames(merged_data) <- merged_data$Row.names
     merged_data$Row.names <- NULL
-  #}else if (identical(data_type, "metadata")){
-  #  merged_data <- merge(data1, data2, by.y="col.names", all=TRUE, suffixes=(c("rep1","rep2")))
-  #  rownames(merged_data) <- merged_data$Row.names
-  #  merged_data$Row.names <- NULL
-  #}else{
-  #  stop(paste("invalid data_type(", data_type, ") please use \"data\" or \"metadata\""))
-  #}
+  }else if (identical(data_type, "metadata")){
+    merged_data <- merge(data1, data2, by.x="col.names", all=TRUE, suffixes=(c("rep1","rep2")))
+    rownames(merged_data) <- merged_data$Row.names
+    merged_data$Row.names <- NULL
+  }else{
+    stop(paste("invalid data_type(", data_type, ") please use \"data\" or \"metadata\""))
+  }
 
   ## # determine if there are dupilcate datasets -- report if there are
   ## duplicate_entries <- duplicated( c( dimnames(data1)[2], dimnames(data2)[2] ) )
@@ -49,17 +49,17 @@ merge_data <- function(mode="file", data_type = "data", file1="", file2="", outp
   ## }
   
   # non optional, convert "na's" to 0
-    merged_data[is.na(merged_data)] <- 0
+  merged_data[is.na(merged_data)] <- 0
   
   # write output
-  if ( identical(mode, "file")==TRUE ){
+  #if ( identical(mode, "file")==TRUE ){
     write.table(merged_data, file = output, col.names=NA, row.names = TRUE, sep="\t", quote=FALSE)
-  }else if (identical(data_type, "metadata")){
-    rot90(rot90(rot90(merged_data)))
-    write.table(merged_data, file = output, col.names=NA, row.names = TRUE, sep="\t", quote=FALSE)
-  }else{
-    stop(paste("invalid data_type(", data_type, ") please use \"data\" or \"metadata\""))
-  }
+  #}else if (identical(data_type, "metadata")){
+  #  rot90(rot90(rot90(merged_data)))
+  #  write.table(merged_data, file = output, col.names=NA, row.names = TRUE, sep="\t", quote=FALSE)
+  #}else{
+  #  stop(paste("invalid data_type(", data_type, ") please use \"data\" or \"metadata\""))
+  #}
     
   
 }
