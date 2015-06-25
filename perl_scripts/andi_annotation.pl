f00d7eb4f733ee8621691af34beeda20ac6b8a57
Kevin Keegan <kevin.p.keegan@gmail.com>
2015-06-25 12:52:15 -0500

    debug

--- perl_scripts/andi_annotation.pl
+++ perl_scripts/andi_annotation.pl
@@ -27,7 +27,7 @@ my $token       = undef ;
 my $date        = `date` ; chomp $date ;
 my $myJobName   = $date ;
 my $user        = 'thulsadoon' ;
-my $project     = undef ;
+my $project     = "vitametagenomics" ;
 my $file        = undef ; #"test.sim_shock_node_list.andi" ;
 
 
